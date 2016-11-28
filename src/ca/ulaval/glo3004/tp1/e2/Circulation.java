61195791aac05ef2009c64e930deeca78bbfefa3
kedzilla <xavierkedzi@gmail.com>
2016-11-28 17:22:55 -0500

    Added thread

--- src/ca/ulaval/glo3004/tp1/e2/Circulation.java
+++ src/ca/ulaval/glo3004/tp1/e2/Circulation.java
@@ -1,41 +1,53 @@
 package ca.ulaval.glo3004.tp1.e2;
 
-public class Circulation {
-	private Voitures voitures;
-	private Pietons pietons;
-	private Intersection intersection;
+import java.util.List;
 
-	public Circulation() {
-		intersection = new Intersection();
-		voitures = new Voitures();
-		pietons = new Pietons();
-	}
+public class Circulation extends Thread {
+    private Voitures voitures;
+    private Pietons pietons;
+    private Intersection intersection;
+    private List<String> commandes;
 
-	public void execute(Commande commande) {
-		System.out.println("Commande en cours: " + commande.reconstruireLaCommande());
-		try {
-			if (commande.estPourLumiere()) {
-				intersection.changerLumieres(commande);
-			} else if (commande.estPourPieton()) {
-				pietons.faireActionPieton(commande, intersection);
-			} else if (commande.estPourVoiture()) {
-				voitures.faireActionVoiture(commande, intersection);
-			} else {
-				throw new CommandeNonCompleteException("Commande invalide : " + commande.reconstruireLaCommande());
-			}
-			System.out.println("La commande a été effectuée avec succès!");
-		} catch (CommandeNonCompleteException e) {
-			System.out.println("La commande n'a pas été effectuée avec succès.");
-			System.out.println(e.getMessage());
-		}
-		printEtatSysteme();
-	}
+    public Circulation(List<String> commandes) {
+        intersection = new Intersection();
+        voitures = new Voitures();
+        pietons = new Pietons();
+        this.commandes = commandes;
+    }
 
-	private void printEtatSysteme() {
-		System.out.println("\nVoici l'état courant du système:");
-		intersection.printEtatLumieres();
-		voitures.printEtatVoitures();
-		pietons.printEtatPietons();
-		System.out.println("\n");
-	}
+    public void run() {
+        for (String action : this.commandes) {
+            Commande commande = new Commande(action);
+            this.execute(commande);
+            printEtatSysteme();
+        }
+        return;
+    }
+
+    public void execute(Commande commande) {
+        System.out.println("Commande en cours: " + commande.reconstruireLaCommande());
+        try {
+            if (commande.estPourLumiere()) {
+                intersection.changerLumieres(commande);
+            } else if (commande.estPourPieton()) {
+                pietons.faireActionPieton(commande, intersection);
+            } else if (commande.estPourVoiture()) {
+                voitures.faireActionVoiture(commande, intersection);
+            } else {
+                throw new CommandeNonCompleteException("Commande invalide : " + commande.reconstruireLaCommande());
+            }
+            System.out.println("La commande a été effectuée avec succès!");
+        } catch (CommandeNonCompleteException e) {
+            System.out.println("La commande n'a pas été effectuée avec succès.");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void printEtatSysteme() {
+        System.out.println("\nVoici l'état courant du système:");
+        intersection.printEtatLumieres();
+        voitures.printEtatVoitures();
+        pietons.printEtatPietons();
+        System.out.println("\n");
+    }
 }
